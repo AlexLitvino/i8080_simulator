ef36a47fcf8a9de15ce7819fe8b1c10e06cd25d9
AlexLitvino <litvinov.aleks@gmail.com>
2015-12-20 23:31:20 +0200

    added Register.py to cover Register's functionality

---
+++ Register.py
@@ -0,0 +1,20 @@
+__author__ = '_Intel_'
+
+
+class Register():
+
+    def __init__(self, name, accumulator, flags):
+        self.name = name
+        self.value = 0
+        self.flags = flags
+
+    def normalization(self):
+        if 0 <= self.value <= 255:
+            pass
+        else:
+            self.value = self.value % 256
+
+    def add(self, summand):
+        self.value = self.value + summand
+        #register flags update
+        self.normalization()
