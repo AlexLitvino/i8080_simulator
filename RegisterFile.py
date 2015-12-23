081baf0285291aafe206034e66b0a968f0fec83d
AlexLitvino <litvinov.aleks@gmail.com>
2015-12-23 23:03:36 +0200

    added file for register manipulation together

---
+++ RegisterFile.py
@@ -0,0 +1,16 @@
+from MiscRegisters import Accumulator
+import Register
+
+
+class RegisterFile():
+
+    def __init__(self):
+        self.A = Accumulator("A")#accumulator
+
+        self.B = Register("B")
+        self.C = Register("C")
+        self.B = Register("D")
+        self.E = Register("E")
+        self.H = Register("H")
+        self.H = Register("L")
+        self.register = []
