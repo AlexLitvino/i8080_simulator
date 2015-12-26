e89de853f0d634c41e3ecee5dd9b10191c357a31
AlexLitvino <litvinov.aleks@gmail.com>
2015-12-26 20:54:09 +0200

    added docstrings to get_bit and get_bits. get_bit fixed by shifting result to right.

--- utilities.py
+++ utilities.py
@@ -1,29 +1,25 @@
 
 
 def get_bit(value, index):
-    return value & (1 << index)
+    """
+    Returns bit in position index from byte value
+    :param value: integer positive value
+    :param index: number of target bit in byte, most-significant bit is considered on the left and has number 0.
+    :return: bit in position index from byte value
+    """
+    return (value & (1 << index)) >> index
+
 
 def get_bits(value, start_index, end_index):
+    """
+    Returns a set of bit in position from start_index till end_index indexes(inclusively) from byte value
+    :param value: integer positive value
+    :param start_index: the number of the first bit which will be included to result;
+    most-significant bit is considered on the left and has number 0.
+    :param end_index: the number of the last bit which will be included to result,
+    end_index should be greater than start_index; most-significant bit is considered on the left and has number 0.
+    :return: set of bit in position from start_index till end_index indexes(inclusively) from byte value
+    """
     mask = 2 ** (end_index - start_index + 1) - 1
-    print("mask::" + "{0:b}".format(mask))
-
     mask = mask << start_index
-    print("mask::" + "{0:b}".format(mask))
-
-
-    temp = value & mask
-    print("temp::" + "{0:b}".format(temp))
-
-    ret_val = temp >> start_index
-    print("ret_val::" + "{0:b}".format(ret_val))
-
-    #print("mask::" + "{0:b}".format(mask))#shift to right
-    #print("mask::" + "{0:b}".format(value & mask >> start_index))
-    return 0
-
-
-a = 0b101
-print("{0:b}".format(a))
-
-#get_bit(a, 4)
-get_bits(a, 1, 8)
+    return (value & mask) >> start_index
