aafeb4ea2266c8e4fa0476f77dbaf0d08cd5d131
AlexLitvino <litvinov.aleks@gmail.com>
2016-01-09 00:33:16 +0200

    added list of i8080 commands

---
+++ commands.py
@@ -0,0 +1,102 @@
+
+
+
+commands = []
+
+inr = "inr"
+dcr = "dcr"
+cma = "cma"
+daa = "daa"
+nop = "nop"
+
+
+mov = "mov"
+stax = "stax"
+ldax = "ldax"
+add = "add"
+adc = "adc"
+sub = "sub"
+sbb = "sbb"
+ana = "ana"
+xra = "xra"
+ora = "ora"
+cmp = "cmp"
+
+
+rlc = "rlc"
+rrc = "rrc"
+ral = "ral"
+rar = "rar"
+
+
+sta = "sta"
+lda = "lda"
+shld = "shld"
+lhld = "lhld"
+cmc = "cmc"
+
+
+push = "push"
+pop = "pop"
+dad = "dad"
+inx = "inx"
+dcx = "dcx"
+xchg = "xchg"
+xthl = "xthl"
+sphl = "sphl"
+pchl = "pchl"
+
+
+lxi = "lxi"
+mvi = "mvi"
+adi = "adi"
+aci = "aci"
+sui = "sui"
+sbi = "sbi"
+ani = "ani"
+xri = "xri"
+ori = "ori"
+cpi = "cpi"
+
+
+in_cmd = "in"#to the command name added _cmd suffix to differ from Python "in" operator
+out = "out"
+ei = "ei"
+di = "di"
+hlt = "hlt"
+rst = "rst"
+stc = "stc"
+
+
+jmp = "jmp"
+jnz = "jnz"
+jz = "jz"
+jnc = "jnc"
+jc = "jc"
+jpo = "jpo"
+jpe = "jpe"
+jp = "jp"
+jm = "jm"
+
+
+ret = "ret"
+rnz = "rnz"
+rz = "rz"
+rnc = "rnc"
+rc = "rc"
+rpo = "rpo"
+rpe = "rpe"
+rp = "rp"
+rm = "rm"
+
+
+call = "call"
+cnz = "cnz"
+cz = "cz"
+cnc = "cnc"
+cc = "cc"
+cpo = "cpo"
+cpe = "cpe"
+cp = "cp"
+cm = "cm"
+
