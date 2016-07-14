5095606181da7adf502d6743a932fd5d4bd9a2af
AlexLitvino <litvinov.aleks@gmail.com>
2016-07-14 22:58:39 +0300

    Corrected register D variable (from B); Added PC, SP, Status registers.

--- register_file.py
+++ register_file.py
@@ -1,16 +1,22 @@
-from misc_registers import Accumulator
+from misc_registers import Accumulator, ProgramCounter, StackPointer, StatusRegister
 from register import Register
 
 
 class RegisterFile():
 
     def __init__(self):
-        self.A = Accumulator("A")#accumulator
 
-        self.B = Register("B")
-        self.C = Register("C")
-        self.B = Register("D")
-        self.E = Register("E")
-        self.H = Register("H")
-        self.L = Register("L")
+        # TODO: None parameters, should be avoided?
+        self.A = Accumulator("A", None, None)#accumulator
+        self.F = StatusRegister()
+
+        self.PC = ProgramCounter()
+        self.SP = StackPointer()
+
+        self.B = Register("B", self.A, self.F)
+        self.C = Register("C", self.A, self.F)
+        self.D = Register("D", self.A, self.F)
+        self.E = Register("E", self.A, self.F)
+        self.H = Register("H", self.A, self.F)
+        self.L = Register("L", self.A, self.F)
         self.register = []
