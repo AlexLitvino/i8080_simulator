fb09efbad10f29b4ce3eb52926eee46e725ea062
AlexLitvino <litvinov.aleks@gmail.com>
2016-07-18 23:43:50 +0300

    Added dump function to print memory content.

--- memory.py
+++ memory.py
@@ -1,4 +1,5 @@
 from constants import MEMORY_SIZE
+from utilities import hex_formatter
 
 
 class Memory():
@@ -18,4 +19,12 @@ class Memory():
         else:
             raise Exception
 
-
+    def dump(self, start_address=0, end_address=16):
+        """
+        Prints memory content along with addresses from start_address to end_address (including boundaries)
+        :param start_address: starting address to print memory content; 0 by default
+        :param end_address: ending address to print memory content; 16 by default
+        :return: None
+        """
+        for address in range(start_address, end_address + 1):
+            print("M[" + hex_formatter(address) + "]:" + hex_formatter(self._memory[address]))
